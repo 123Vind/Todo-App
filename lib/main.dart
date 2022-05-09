fb51cebc3a170c513297ebb792f8f20c5269e16d
vind <me@example.com>
2022-05-09 14:28:05 +0530

    slider position change with container

--- lib/main.dart
+++ lib/main.dart
@@ -11,19 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
       home: const HomePage(),
     );
   }
@@ -37,42 +24,55 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double posit = 0.0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Todo App'),
+        title: Text('slide Container'),
       ),
-      body: GridView(
-        gridDelegate:
-            const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-        children: const [
-          _boxcard(),
-          _boxcard(),
-          _boxcard(),
-          _boxcard(),
-        ],
+      body: SafeArea(
+        child: Container(
+          child: Stack(
+            children: [
+              Slider(
+                value: posit,
+                max: 100,
+                divisions: 100,
+                label: posit.round().toString(),
+                onChanged: (double value) {
+                  setState(() {
+                    posit = value;
+                  });
+                },
+              ),
+              Positioned(
+                left: posit * 8,
+                child: cardCon(),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-// ignore: camel_case_types
-class _boxcard extends StatefulWidget {
-  const _boxcard({Key? key}) : super(key: key);
+class cardCon extends StatefulWidget {
+  const cardCon({Key? key}) : super(key: key);
 
   @override
-  State<_boxcard> createState() => __boxcardState();
+  State<cardCon> createState() => _cardConState();
 }
 
-// ignore: camel_case_types
-class __boxcardState extends State<_boxcard> {
+class _cardConState extends State<cardCon> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 200,
-      width: 200,
-      color: Colors.amber,
+      height: 100,
+      width: 100,
+      decoration: BoxDecoration(
+          color: Colors.amber, borderRadius: BorderRadius.circular(10)),
     );
   }
 }
